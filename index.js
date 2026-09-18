6b8d2b6c81b44538b2632407f6adf55470c16a27
gunn1104 <gunrawin@gmail.com>
2026-09-18 13:26:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'https://esm.sh/react@18';
+import ReactDOM from 'https://esm.sh/react-dom@18';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  React.createElement(App)
+);
