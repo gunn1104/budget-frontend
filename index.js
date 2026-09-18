84ed7ce8c5b292219c5b88fac643024604bfd198
gunn1104 <gunrawin@gmail.com>
2026-09-18 13:53:19 +0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'https://esm.sh/react@18';
-import ReactDOM from 'https://esm.sh/react-dom@18';
-import App from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(App)
-);
